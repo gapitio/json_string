6b42bac401fb3d7ddb6f45f58e977c53a872b7d9
Amir Abdin <amab8901@protonmail.com>
2024-12-16 16:43:05 +0100

    feat: add stringified json array & stringified json string

--- src/json_array.rs
+++ src/json_array.rs
@@ -1,14 +1,27 @@
 use crate::json_parse_approach::{parse_json_string, JsonContext};
 
-pub fn prepare_json_string(original_str: &str) -> String {
+pub fn prepare_json_array(original_str: &str) -> String {
     let trimmed_str = original_str.trim_matches([' ', '\n', '\t', ',', ';', ':']);
-    let json_context = json_context(trimmed_str);
-    let content_str = content_str(json_context.clone(), trimmed_str);
+
+    let array_input = ensure_array_wrapper(trimmed_str);
+
+    let json_context = json_context(&array_input);
+    let content_str = content_str(json_context.clone(), &array_input);
 
     let parsed_json_string = parse_json_string(&content_str, json_context.clone());
 
-    let rewrapped_string = rewrap_string(&parsed_json_string, trimmed_str, json_context);
+    let rewrapped_string = rewrap_string(&parsed_json_string, json_context);
+
     rewrapped_string
+}
+
+pub(crate) fn ensure_array_wrapper(string: &str) -> String {
+    let array_string = if string.starts_with('[') && string.ends_with(']') {
+        string.to_string()
+    } else {
+        format!("[{string}]")
+    };
+    array_string
 }
 
 #[allow(clippy::needless_pass_by_value)]
@@ -45,11 +58,7 @@ pub(crate) fn json_context(trimmed_str: &str) -> JsonContext {
 }
 
 #[allow(clippy::needless_pass_by_value)]
-pub(crate) fn rewrap_string(
-    parsed_json_string: &str,
-    trimmed_str: &str,
-    json_context: JsonContext,
-) -> String {
+pub(crate) fn rewrap_string(parsed_json_string: &str, json_context: JsonContext) -> String {
     let rewrapped_string = match json_context {
         JsonContext::Array => {
             let rewrapped_string = format!("[{parsed_json_string}]");
@@ -61,7 +70,7 @@ pub(crate) fn rewrap_string(
 
             rewrapped_string
         }
-        JsonContext::Value => trimmed_str.to_string(),
+        JsonContext::Value => parsed_json_string.to_string(),
     };
 
     rewrapped_string
@@ -69,17 +78,17 @@ pub(crate) fn rewrap_string(
 
 #[cfg(test)]
 mod tests {
-    use crate::prepare_json_string;
+    use crate::json_array::prepare_json_array;
 
     #[test]
-    fn array_with_multiple_objects_and_semicolon_separator() {
+    fn array_with_multiple_objects_and_semicolon_separator_to_array() {
         let original_str = r#"[
             {"Foo1":"BAR1", "Foo2":"BAR2", "Foo3":"BAR3"};
             {"Foo4":"BAR4", "Foo5":"BAR5", "Foo6":"BAR6"};
             {"Foo7":"BAR7", "Foo8":"BAR8", "Foo9":"BAR9"}
         ]"#;
 
-        let prepared_str = prepare_json_string(original_str);
+        let prepared_str = prepare_json_array(original_str);
 
         let expected_str = r#"[{"Foo1": "BAR1", "Foo2": "BAR2", "Foo3": "BAR3"}, {"Foo4": "BAR4", "Foo5": "BAR5", "Foo6": "BAR6"}, {"Foo7": "BAR7", "Foo8": "BAR8", "Foo9": "BAR9"}]"#;
 
@@ -87,14 +96,14 @@ mod tests {
     }
 
     #[test]
-    fn array_with_multiple_objects() {
-        let original_str = r#"[
+    fn array_with_multiple_objects_to_array() {
+        let original_str = r#"
             {"Foo1":"BAR1", "Foo2":"BAR2", "Foo3":"BAR3"},
             {"Foo4":"BAR4", "Foo5":"BAR5", "Foo6":"BAR6"},
-            {"Foo7":"BAR7", "Foo8":"BAR8", "Foo9":"BAR9"}
-        ]"#;
+            {"Foo7":"BAR7", "Foo8":"BAR8", "Foo9":"BAR9"};
+        "#;
 
-        let prepared_str = prepare_json_string(original_str);
+        let prepared_str = prepare_json_array(original_str);
 
         let expected_str = r#"[{"Foo1": "BAR1", "Foo2": "BAR2", "Foo3": "BAR3"}, {"Foo4": "BAR4", "Foo5": "BAR5", "Foo6": "BAR6"}, {"Foo7": "BAR7", "Foo8": "BAR8", "Foo9": "BAR9"}]"#;
 
@@ -102,13 +111,13 @@ mod tests {
     }
 
     #[test]
-    fn prepare_json_string_w_empty_groups() {
+    fn prepare_json_array_w_empty_groups_to_array() {
         let original_str = r#"
             [
                 {"tag":"lol", "groups":[]}
             ]
         "#;
-        let prepared_str = prepare_json_string(original_str);
+        let prepared_str = prepare_json_array(original_str);
 
         let expected_str = r#"[{"tag": "lol", "groups": []}]"#;
 
@@ -116,13 +125,13 @@ mod tests {
     }
 
     #[test]
-    fn prepare_json_string_w_inner_json() {
+    fn prepare_json_array_w_inner_json_to_array() {
         let original_str = r#"
             [
                 {"label":"lol", "customtags": {"k1": "v1"}}
             ]
         "#;
-        let prepared_str = prepare_json_string(original_str);
+        let prepared_str = prepare_json_array(original_str);
 
         let expected_str = r#"[{"label": "lol", "customtags": {"k1": "v1"}}]"#;
 
@@ -130,28 +139,28 @@ mod tests {
     }
 
     #[test]
-    fn entirely_without_quotes() {
+    fn entirely_without_quotes_to_array() {
         let original_str = "{
             property1: lol,
             property2: iskrem
         }";
 
-        let prepared_str = prepare_json_string(original_str);
+        let prepared_str = prepare_json_array(original_str);
 
-        let expected_str = "{\"property1\": \"lol\", \"property2\": \"iskrem\"}";
+        let expected_str = "[{\"property1\": \"lol\", \"property2\": \"iskrem\"}]";
 
         assert_eq!(prepared_str, expected_str);
     }
 
     #[test]
-    fn arbitrary_test() {
+    fn arbitrary_test_to_array() {
         let original_str = r#"{
             [{"unit_id": 5,"ec_id": 0,"label": "SOMETHING","uid": "00000000-0000-0000-0000-000000000001", "customtags": {"tag1": ""}, "groups": ["input:max_adr:205"] }]
         }"#;
 
-        let prepared_str = prepare_json_string(original_str);
+        let prepared_str = prepare_json_array(original_str);
 
-        let expected_str = r#"{[{"unit_id": 5, "ec_id": 0, "label": "SOMETHING", "uid": "00000000-0000-0000-0000-000000000001", "customtags": {"tag1": ""}, "groups": ["input:max_adr:205"]}]}"#.to_string();
+        let expected_str = r#"[{[{"unit_id": 5, "ec_id": 0, "label": "SOMETHING", "uid": "00000000-0000-0000-0000-000000000001", "customtags": {"tag1": ""}, "groups": ["input:max_adr:205"]}]}]"#.to_string();
 
         assert_eq!(prepared_str, expected_str);
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+pub mod json_array;
 pub mod json_parse_approach;
 pub mod json_string;
+pub mod stringified_json_array;
 pub mod stringified_json_string;
 
+pub use json_array::prepare_json_array;
 pub use json_string::prepare_json_string;
+pub use stringified_json_array::prepare_stringified_json_array;
 pub use stringified_json_string::prepare_stringified_json_string;
